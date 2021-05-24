7033ac786afb5a94de4b1ef842fd29517dbc78a9
bvenk <bvekataprudhvi@gmail.com>
2021-05-24 15:37:38 +0530

    Initial commit

---
+++ src/main/java/com/springclient/client/practice/ClientPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springclient.client.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springclient/client/practice/ClientPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springclient.client.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
